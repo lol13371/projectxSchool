a1345d5028497374f4122f795708dd542eae4af8
lol13371 <117176678+lol13371@users.noreply.github.com>
2022-12-04 08:21:36 +0600

    Update main.b607e494.chunk.js

--- static/js/main.b607e494.chunk.js
+++ static/js/main.b607e494.chunk.js
@@ -662,7 +662,7 @@
                                             children: ["ENDS ", Object(N.jsx)("span", {
                                                 className: "text-primary",
                                                 
-                                                children: "03 DEC 12:00H"
+                                                children: "03 DEC 03:00H"
                                             })]
                                         }), Object(N.jsxs)("p", {
                                             className: "priceSale mt-3",
